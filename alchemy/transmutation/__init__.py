4c04de5421008a3ba09df28ad4c5bf8bc59413a8
69Nesta <petitromeo74@gmail.com>
2026-02-05 18:32:44 +0100

    Feat: third part

---
+++ alchemy/transmutation/__init__.py
@@ -0,0 +1,14 @@
+from .basic import lead_to_gold, stone_to_gem
+from .advanced import philosophers_stone, elixir_of_life
+
+
+__version__ = '1.0.0'
+__author__ = 'Some Body'
+
+
+__all__ = [
+    'lead_to_gold',
+    'stone_to_gem',
+    'philosophers_stone',
+    'elixir_of_life'
+]
